ceaf4eb30a1ab9d160a423dd83cded66110a413c
Merei-lab <mereikoilibaiuly@gmail.com>
2026-05-22 16:18:41 +0500

    npm i

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const prisma = require('../lib/prisma.js')
 const bcrypt = require('bcrypt')
 class UserController {
     async getUser(req, res) {
