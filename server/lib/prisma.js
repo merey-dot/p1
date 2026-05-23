5ee194b3577679ab0a9e58a68cb9ff3d1e2cdcba
Merei-lab <mereikoilibaiuly@gmail.com>
2026-05-23 18:20:52 +0500

    prisma

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,7 +1,10 @@
 const { PrismaClient } = require('@prisma/client')
 const { PrismaPg } = require('@prisma/adapter-pg')
-const pg = require('pg')
+const { Pool } = require('pg')
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
+
 const prisma = new PrismaClient({ adapter })
+
+module.exports = prisma
