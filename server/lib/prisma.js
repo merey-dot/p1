be0c25b47c2ba302b2fdbe7cb272bc671203944e
Merei-lab <mereikoilibaiuly@gmail.com>
2026-05-23 20:12:42 +0500

    prisma

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,10 +1,23 @@
 const { PrismaClient } = require('@prisma/client')
 const { PrismaPg } = require('@prisma/adapter-pg')
 const { Pool } = require('pg')
-console.log('db', process.env.DATABASE_URL)
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+const connectionString = process.env.DATABASE_URL
+
+console.log("DATABASE_URL exists:", !!connectionString)
+
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false   // ← обязательно для Render
+    }
+})
+
 const adapter = new PrismaPg(pool)
 
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({
+    adapter,
+    log: ['error', 'warn']
+})
 
 module.exports = prisma
